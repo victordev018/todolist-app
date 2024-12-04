92cc9b188e599602f58bd04c64fa622e3553d7e3
João VIctor <joawrt8520@gmail.com>
2024-12-04 01:41:02 -0300

    feat: add delete action in list of the tasks

--- task/task.js
+++ task/task.js
@@ -29,16 +29,52 @@ const loadTasks = async () => {
 function fillList(data) {
 
     if(data.length == 0) {
-        listTask.innerHTML = `<span class="no-task"> sem tarefas cadastradas </span>`
+        listTask.innerHTML = `<span class="no-task"> sem tarefas cadastradas </span>`;
+        return;
     }
+
+    listTask.innerHTML = ``;
 
     data.forEach(task => {
         
         listTask.innerHTML += `
-        <li>${task.content}</li>
+        <li class= "task-item">
+            <button class="bt-icon">
+                <img src = "/assets/icons/edit-icon.png" class="icon">
+            </button>
+            <span class="task-content-text">${task.content}</span>
+            <button class="bt-icon" onClick="deleteTaskById(${task.id})">
+                <img src = "/assets/icons/delete-icon.png" class="icon">
+            </button>
+        </li>
         `
 
     });
+}
+
+const deleteTaskById = async (id) => {
+
+    console.log("entrei");
+
+    const token = localStorage.getItem('token');
+    try {
+
+        const response = await fetch(BASE_URL+`/tasks/delete/${id}`, {
+            method: "DELETE",
+            headers:{
+                'Content-Type':'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        });
+
+        if(response.ok){
+            console.log(`deletado com sucesso task: ${id}`);
+            await loadTasks();
+        }
+
+    } catch (error){
+        alert(`Error to connect server ${error.message}`);
+    }
 }
 
 loadTasks();
